5c600c768f8b3e12614625adf09799fde96ec910
Wesley Donker <DonkerNet@users.noreply.github.com>
2021-01-18 15:26:37 +0100

    Improved ConversionUtil so that object can be converted explicitly Added StringUtil for converting PascalCase to snake_case

--- Source/Donker.Extensions/Utils/ConversionUtil.cs
+++ Source/Donker.Extensions/Utils/ConversionUtil.cs
@@ -10,21 +10,91 @@ namespace Donker.Extensions.Utils
     public static class ConversionUtil
     {
         // Converter methods for types that cannot be converted with System.Convert.ChangeType
-        private static readonly IDictionary<Type, Func<object, object>> Converters;
+        private static readonly IDictionary<Type, Func<object, IFormatProvider, object>> Converters;
 
         static ConversionUtil()
         {
-            Converters = new Dictionary<Type, Func<object, object>>
+            Converters = new Dictionary<Type, Func<object, IFormatProvider, object>>
             {
-                { typeof(Guid), obj => Guid.Parse(obj.ToString()) },
-                { typeof(TimeSpan), obj => TimeSpan.Parse(obj.ToString()) },
-                { typeof(Uri), obj => new Uri(obj.ToString()) },
-                { typeof(IPAddress), obj => IPAddress.Parse(obj.ToString()) }
-                // Place additional conversion methods here
+                { typeof(Guid), (obj, fp) => ToGuid(obj) },
+                { typeof(TimeSpan), (obj, fp) => ToTimeSpan(obj, fp) },
+                { typeof(Uri), (obj, fp) => ToUri(obj) },
+                { typeof(IPAddress), (obj, fp) => ToIpAddress(obj) }
             };
         }
 
-        public static object ChangeType(object input, Type newType)
+        public static Guid ToGuid(object input)
+        {
+            if (input is Guid)
+                return (Guid)input;
+
+            byte[] inputBytes = input as byte[];
+            if (inputBytes != null)
+                return new Guid(inputBytes);
+
+            string inputString = input as string;
+            if (inputString != null)
+                return Guid.Parse(inputString);
+
+            throw new ArgumentException($"Input type {input?.GetType() ?? typeof(object)} is not convertible to {typeof(Guid)}.", nameof(input));
+        }
+
+        public static TimeSpan ToTimeSpan(object input, IFormatProvider formatProvider)
+        {
+            if (input is TimeSpan)
+                return (TimeSpan)input;
+
+            if (input is long)
+                return new TimeSpan((long)input);
+
+            string inputString = input as string;
+            if (inputString != null)
+            {
+                return formatProvider != null
+                    ? TimeSpan.Parse(inputString, formatProvider)
+                    : TimeSpan.Parse(inputString);
+            }
+
+            throw new ArgumentException($"Input type {input?.GetType() ?? typeof(object)} is not convertible to {typeof(TimeSpan)}.", nameof(input));
+        }
+
+        public static TimeSpan ToTimeSpan(object input)
+        {
+            return ToTimeSpan(input, null);
+        }
+
+        public static Uri ToUri(object input)
+        {
+            if (input is Uri)
+                return (Uri)input;
+
+            string inputString = input as string;
+            if (inputString != null)
+                return new Uri(input.ToString());
+
+            throw new ArgumentException($"Input type {input?.GetType() ?? typeof(object)} is not convertible to {typeof(Uri)}.", nameof(input));
+        }
+
+        public static IPAddress ToIpAddress(object input)
+        {
+            if (input is IPAddress)
+                return (IPAddress)input;
+
+            byte[] inputBytes = input as byte[];
+            if (inputBytes != null)
+                return new IPAddress(inputBytes);
+
+            if (input is long)
+                return new IPAddress((long)input);
+
+            string inputString = input as string;
+            if (inputString != null)
+                return IPAddress.Parse(inputString);
+
+            throw new ArgumentException($"Input type {input?.GetType() ?? typeof(object)} is not convertible to {typeof(IPAddress)}.", nameof(input));
+        }
+
+        public static object ChangeType(object input, Type newType, IFormatProvider formatProvider)
         {
             if (input == null || newType == null)
                 return null;
@@ -39,20 +109,30 @@ namespace Donker.Extensions.Utils
                 newType = newType.GetEnumUnderlyingType();
             }
 
-            Func<object, object> converter;
+            Func<object, IFormatProvider, object> converter;
 
             if (Converters.TryGetValue(newType, out converter))
-                return converter.Invoke(input);
+                return converter.Invoke(input, formatProvider);
 
             return Convert.ChangeType(input, newType);
         }
 
-        public static T ChangeType<T>(object input)
+        public static object ChangeType(object input, Type newType)
         {
-            return (T)ChangeType(input, typeof(T));
+            return ChangeType(input, newType, null);
         }
 
-        public static bool TryChangeType(object input, Type newType, out object result, out Exception exception)
+        public static T ChangeType<T>(object input, IFormatProvider formatProvider)
+        {
+            return (T)ChangeType(input, typeof(T), formatProvider);
+        }
+
+        public static T ChangeType<T>(object input)
+        {
+            return ChangeType<T>(input, null);
+        }
+
+        public static bool TryChangeType(object input, Type newType, IFormatProvider formatProvider, out object result, out Exception exception)
         {
             if (input == null || newType == null)
             {
@@ -77,10 +157,10 @@ namespace Donker.Extensions.Utils
                     newType = newType.GetEnumUnderlyingType();
                 }
 
-                Func<object, object> converter;
+                Func<object, IFormatProvider, object> converter;
 
                 result = Converters.TryGetValue(newType, out converter)
-                    ? converter.Invoke(input)
+                    ? converter.Invoke(input, formatProvider)
                     : Convert.ChangeType(input, newType);
 
                 exception = null;
@@ -94,16 +174,16 @@ namespace Donker.Extensions.Utils
             }
         }
 
-        public static bool TryChangeType(object input, Type newType, out object result)
+        public static bool TryChangeType(object input, Type newType, IFormatProvider formatProvider, out object result)
         {
             Exception exception;
-            return TryChangeType(input, newType, out result, out exception);
+            return TryChangeType(input, newType, formatProvider, out result, out exception);
         }
 
-        public static bool TryChangeType<T>(object input, out T result, out Exception exception)
+        public static bool TryChangeType<T>(object input, IFormatProvider formatProvider, out T result, out Exception exception)
         {
             object resultObj;
-            if (TryChangeType(input, typeof (T), out resultObj, out exception))
+            if (TryChangeType(input, typeof (T), formatProvider, out resultObj, out exception))
             {
                 result = (T)resultObj;
                 return true;
@@ -113,10 +193,30 @@ namespace Donker.Extensions.Utils
             return false;
         }
 
-        public static bool TryChangeType<T>(object input, out T result)
+        public static bool TryChangeType<T>(object input, IFormatProvider formatProvider, out T result)
         {
             Exception exception;
-            return TryChangeType<T>(input, out result, out exception);
+            return TryChangeType<T>(input, formatProvider, out result, out exception);
+        }
+
+        public static bool TryChangeType(object input, Type newType, out object result, out Exception exception)
+        {
+            return TryChangeType(input, newType, null, out result, out exception);
+        }
+
+        public static bool TryChangeType(object input, Type newType, out object result)
+        {
+            return TryChangeType(input, newType, null, out result);
+        }
+
+        public static bool TryChangeType<T>(object input, out T result, out Exception exception)
+        {
+            return TryChangeType<T>(input, null, out result, out exception);
+        }
+
+        public static bool TryChangeType<T>(object input, out T result)
+        {
+            return TryChangeType<T>(input, null, out result);
         }
     }
 }
---
+++ Source/Donker.Extensions/Utils/StringUtil.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+
+namespace Donker.Extensions.Utils
+{
+    public static class StringUtil
+    {
+        public static string PascalCaseToSnakeCase(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return value;
+
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                char c = value[i];
+
+                if (i > 0 && char.IsUpper(c))
+                    builder.Append('_');
+
+                builder.Append(char.ToLower(c));
+            }
+
+            return builder.ToString();
+        }
+
+        public static string CamelCaseToSnakeCase(string value)
+        {
+            return PascalCaseToSnakeCase(value); // Does the same thing
+        }
+
+        public static string SnakeCaseToPascalCase(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return value;
+
+            StringBuilder builder = new StringBuilder();
+
+            bool nextIsUpper = true;
+
+            foreach (char c in value)
+            {
+                if (nextIsUpper)
+                {
+                    builder.Append(char.ToUpper(c));
+                    nextIsUpper = false;
+                }
+                else if (c == '_')
+                {
+                    nextIsUpper = true;
+                }
+                else
+                {
+                    builder.Append(c);
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        public static string SnakeCaseToCamelCase()
+        {
+
+        }
+    }
+}
